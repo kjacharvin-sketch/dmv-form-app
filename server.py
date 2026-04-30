2ac29fdfd2432d3d4b3b9731ab0d63e354d9ac4a
kjacharvin-sketch <kjacharvin@gmail.com>
2026-04-29 22:29:38 -0400

    Update server.py

--- server.py
+++ server.py
@@ -12,4 +12,5 @@ def generate():
     return {"status": "working"}
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
